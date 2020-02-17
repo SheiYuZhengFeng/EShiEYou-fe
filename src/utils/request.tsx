138a3c6141426454955a355f11bbb88423f1b3ea
chenghaopeng <i@chper.cn>
2020-02-17 15:36:41 +0800

    修改any为{}

--- src/utils/request.tsx
+++ src/utils/request.tsx
@@ -1,19 +1,20 @@
 import axios from "axios";
+import { isUndefined } from "util";
 
 export interface SimpleResponse {
   code: number,
-  data: any,
+  data: {},
 }
 
 const server = "https://mockapi.eolinker.com/xst28TE9fd8c9df6f852cffd7f835a68febaceb5100bc4d/user/logout";
 
-const withToken = (body : any) => {
-  if (!body) body = {};
+const withToken = (body : {} | undefined) => {
+  if (isUndefined(body)) body = {};
   if (!window.localStorage.token) return body;
   return {...body, token: window.localStorage.token};
 }
 
-export default async function request(url : string, body ?: any, token = true) : Promise<SimpleResponse> {
+export default async function request(url : string, body ?: {}, token = true) : Promise<SimpleResponse> {
   if (token) body = withToken(body);
   return await axios({
     method: "POST",
