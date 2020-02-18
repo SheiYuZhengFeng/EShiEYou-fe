5c270a495b70e456c2a5ed4f53bba4a8f9aa16f6
chenghaopeng <i@chper.cn>
2020-02-18 13:28:13 +0800

    中教 api 完成

---
+++ src/services/NativeAPI.tsx
@@ -0,0 +1,37 @@
+import request, { SimpleResponse } from "../utils/request";
+
+const prefix = "/native";
+
+const MainAPI = {
+  register(body: {username: string, name: string, password: string, sex: number, age: number, content: string, phone: string, qualification: string, background: string}) : Promise<SimpleResponse> {
+    return request(prefix + "/register", body, false);
+  },
+  me() : Promise<SimpleResponse | {data: {username: string, name: string, sex: number, age: number, createtime: number, content: string, phone: string, qualification: string, background: string, payment: number, time: string}}> {
+    return request(prefix + "/me");
+  },
+  edit(body: {content: string, time: string}) : Promise<SimpleResponse> {
+    return request(prefix + "/edit", body);
+  },
+};
+
+const OrderAPI = {
+  list() : Promise<SimpleResponse | {data: {orders: {id: number, condition: number, cid: number, student: number, createtime: number, starttime: number, endtime: number}[]}}> {
+    return request(prefix + "/order/list");
+  },
+  reply(body: {id: number, accept: boolean}) : Promise<SimpleResponse> {
+    return request(prefix + "/order/reply", body);
+  },
+};
+
+const BillAPI = {
+  list() : Promise<SimpleResponse | {data: {bills: {id: number, cost: number, createtime: number}[]}}> {
+    return request(prefix + "/bill/list");
+  },
+  pay(body: {cost: number}) : Promise<SimpleResponse> {
+    return request(prefix + "/bill/pay", body);
+  },
+};
+
+const NativeAPI = {main: MainAPI, bill: BillAPI, order: OrderAPI};
+
+export default NativeAPI;
