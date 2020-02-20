241d3907717945abe8dc92e9baae5a773c287558
chenghaopeng <i@chper.cn>
2020-02-20 10:38:33 +0800

    引入路由；添加主页

---
+++ src/router/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { BrowserRouter, Switch } from "react-router-dom";
+import RouteList from "./config";
+
+export default function() {
+  return (
+    <BrowserRouter>
+      <Switch>
+        { RouteList }
+      </Switch>
+    </BrowserRouter>
+  );
+}
