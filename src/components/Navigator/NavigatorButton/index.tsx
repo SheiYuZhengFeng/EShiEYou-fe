088e98168d392cc7e221362c42f8cfeac0c57942
chenghaopeng <i@chper.cn>
2020-02-22 22:52:06 +0800

    导航栏

--- src/components/Navigator/NavigatorButton/index.tsx
+++ src/components/Navigator/NavigatorButton/index.tsx
@@ -1,17 +1,26 @@
 import React from 'react';
 import styles from './index.module.less';
 import { Tooltip, Icon } from 'antd';
+import { withRouter, RouteComponentProps } from 'react-router';
+import { NavLink } from 'react-router-dom';
 
-class NavigatorButton extends React.Component<{href: string, title: string, type: string}> {
+class NavigatorButton extends React.Component<{href: string, title: string, type: string} & RouteComponentProps> {
+  
   render() {
+    let iconClass = styles.icon;
+    if (this.props.location.pathname.indexOf(this.props.href.toString()) !== -1 && !(this.props.href.toString() === "/" && this.props.location.pathname !== "/")) {
+      iconClass += " " + styles.activeIcon;
+    }
     return (
-      <div className={styles.whole}>
-        <Tooltip title={this.props.title} placement={"bottom"}>
-            <Icon type={this.props.type} theme="filled" />
-        </Tooltip>
-      </div>
+      <NavLink to={this.props.href}>
+        <div className={styles.whole}>
+          <Tooltip title={this.props.title} placement={"top"}>
+            <Icon className={iconClass} type={this.props.type} />
+          </Tooltip>
+        </div>
+      </NavLink>
     );
   }
 }
 
-export default NavigatorButton;
+export default withRouter(NavigatorButton);
--- src/components/Navigator/index.tsx
+++ src/components/Navigator/index.tsx
@@ -5,8 +5,12 @@ import NavigatorButton from './NavigatorButton';
 class Navigator extends React.Component {
   render() {
     return (
-      <div className={styles.box}>
+      <div className={`${styles.box} ${styles.whole}`}>
         <NavigatorButton href='/' title='主页' type='home' />
+        <NavigatorButton href='/course' title='课程' type='book' />
+        <NavigatorButton href='/order' title='预约' type='bulb' />
+        <NavigatorButton href='/mail' title='私信' type='message' />
+        <NavigatorButton href='/user' title='个人' type='user' />
       </div>
     );
   }
