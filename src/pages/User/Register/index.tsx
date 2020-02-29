20d0b9d757eb4dd5401cb3a31884798905d0a230
chenghaopeng <i@chper.cn>
2020-02-29 17:24:22 +0800

    注册完成

--- src/pages/User/Register/index.tsx
+++ src/pages/User/Register/index.tsx
@@ -1,23 +1,98 @@
 import React from 'react';
 import styles from './index.module.less';
-import { Select, Input, Button, Icon, message, Tooltip } from 'antd';
+import { Select, Input, Button, Icon, message, Tooltip, Modal } from 'antd';
 import { toLoginAction } from '../../../actions/UserAction';
 import QueueAnim from 'rc-queue-anim';
+import StudentAPI from '../../../services/StudentAPI';
+import NativeAPI from '../../../services/NativeAPI';
+import ForeignAPI from '../../../services/ForeignAPI';
 
 const { Option } = Select;
 
-class Register extends React.Component<{}, {category: number}> {
+class Register extends React.Component<{}, {category: number, loading: boolean}> {
   form: any = {category: 0}
   constructor(props: {}) {
     super(props);
-    this.state = {category: 0};
+    this.state = {category: 0, loading: false};
+  }
+  validate = () => {
+    const { form } = this;
+    let msg = [];
+    if (!form.username || !form.password || !form.repeat) msg.push("请输入用户名和密码");
+    if (form.password !== form.repeat) msg.push("请确认两次密码输入一致");
+    if (form.sex === undefined) msg.push("请选择你的性别");
+    if (form.age === undefined) msg.push("请选择你的年龄");
+    if (!form.name) msg.push("请输入你的真实姓名");
+    if (!form.phone) msg.push("请输入你的手机号");
+    if (!form.content) msg.push("请输入你的个人简介");
+    if (form.language === undefined) msg.push("请选择你的语种");
+    if (form.category === 0) {
+      if (form.level === undefined) msg.push("请选择你目前的语言水平");
+      if (form.target === undefined) msg.push("请选择你希望锻炼的能力");
+    }
+    else {
+      if (!form.qualification) msg.push("请输入你的语言资质");
+      if (!form.background) msg.push("请输入你的高校与学历");
+      if (form.category === 2) {
+        if (!form.resume) msg.push("请填写你的简历");
+      }
+    }
+    if (msg.length > 0) {
+      Modal.warning({
+        title: "你的注册表单需要完善",
+        content: msg.map((v, i) => <p key={i} style={{margin: "0"}}>{v}</p>),
+        okText: "好的",
+      });
+      return false;
+    }
+    return true;
+  }
+  submit = () => {
+    const { form } = this;
+    let nf = {
+      username: form.username,
+      password: form.password,
+      sex: form.sex,
+      age: form.age,
+      name: form.name,
+      phone: form.phone,
+      language: form.language,
+      content: form.content,
+    };
+    if (form.category === 0) {
+      return StudentAPI.main.register({...nf, level: form.level, target: form.target});
+    }
+    if (form.category === 1) {
+      return NativeAPI.main.register({...nf, qualification: form.qualification, background: form.background});
+    }
+    if (form.category === 2) {
+      return ForeignAPI.main.register({...nf, qualification: form.qualification, background: form.background, resume: form.resume});
+    }
   }
   handleRegister = () => {
-    console.log(this.form);
-    if (!this.form.username || !this.form.password) {
-      message.error("请输入用户名和密码！");
-      return;
+    if (!this.validate()) return;
+    const onOk = () => {
+      const hide = message.loading("正在注册...", 0);
+      this.setState({...this.state, loading: true});
+      this.submit()?.then(res => {
+        hide();
+        this.setState({...this.state, loading: false});
+        if (res.code === 0) {
+          message.success("注册成功！");
+          toLoginAction();
+        }
+        else {
+          message.error("注册失败，请检查你的注册单！");
+        }
+      });
     }
+    Modal.confirm({
+      title: "即将提交注册",
+      content: "请再次确认你的信息，提交后我们将进行验证。",
+      okText: "提交",
+      cancelText: "返回再次确认",
+      onOk: onOk,
+    });
   }
   handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     this.form[e.target.name] = e.target.value.trim();
@@ -86,8 +161,8 @@ class Register extends React.Component<{}, {category: number}> {
             <Input key="4" name="resume" prefix={<Icon type="profile"/>} placeholder="简历" style={inputStyle} onChange={this.handleChange} />
           : null}
         </QueueAnim>
-        <Button type="primary" htmlType="submit" style={inputStyle} onClick={this.handleRegister}>注册</Button>
-        <Button style={inputStyle} onClick={toLoginAction}>登录</Button>
+        <Button type="primary" htmlType="submit" style={inputStyle} onClick={this.handleRegister} loading={this.state.loading}>注册</Button>
+        <Button style={inputStyle} onClick={toLoginAction} disabled={this.state.loading}>登录</Button>
       </div>
     );
   }
