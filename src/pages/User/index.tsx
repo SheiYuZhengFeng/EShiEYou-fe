5e9c78d0a8f328b137787cc67f785fffa9b511f9
chenghaopeng <i@chper.cn>
2020-03-01 21:55:56 +0800

    修改个人信息

--- src/pages/User/index.tsx
+++ src/pages/User/index.tsx
@@ -4,26 +4,27 @@ import Combiner from '../../components/Combiner';
 import store from '../../store';
 import Login from './Login';
 import Register from './Register';
-import { Spin, Avatar, Switch, Button, Modal, message, Input, Icon } from 'antd';
+import { Spin, Avatar, Switch, Button, Modal, message, Input, Icon, Select } from 'antd';
 import StudentAPI from '../../services/StudentAPI';
 import NativeAPI from '../../services/NativeAPI';
 import ForeignAPI from '../../services/ForeignAPI';
 import QueueAnim from 'rc-queue-anim';
 import { informationAction, LogoutAction } from '../../actions/UserAction';
-import UserDescriptions from '../../components/UserDescriptions';
+import UserDescriptions, { CONST } from '../../components/UserDescriptions';
 import GeneralAPI from '../../services/GeneralAPI';
 
-class User extends React.Component<{}, {loged: boolean, view: number, information: any, expand: boolean, password: boolean}> {
+class User extends React.Component<{}, {loged: boolean, view: number, information: any, expand: boolean, password: boolean, modify: boolean}> {
   form: any = {}
   constructor(props: any) {
     super(props);
-    this.state = {information: {}, expand: false, password: false, ...store.getState().UserReducer};
+    this.state = {information: {}, expand: false, password: false, modify: false, ...store.getState().UserReducer};
   }
   ss = store.subscribe(() => {
     this.setState({...this.state, ...store.getState().UserReducer});
   });
   onSwitch = (checked: boolean) => {
     this.setState({...this.state, expand: checked});
+    if (checked) this.form = {...this.state.information};
   }
   setting = () => {
     if (!this.state.expand) return null;
@@ -46,6 +47,9 @@ class User extends React.Component<{}, {loged: boolean, view: number, informatio
     const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
       this.form[e.target.name] = e.target.value.trim();
     }
+    const handleSelect = (name: string, value: number) => {
+      this.form[name] = value;
+    }
     const handlePassword = () => {
       const { form } = this;
       if (!form.oldpassword || !form.newpassword || !form.repeat || form.newpassword !== form.repeat) {
@@ -67,8 +71,47 @@ class User extends React.Component<{}, {loged: boolean, view: number, informatio
       });
       this.form = {};
     }
+    const { category } = store.getState().UserReducer.session;
+    const handleModify = () => {
+      const { form } = this;
+      let update;
+      const hide = message.loading("正在修改...", 0);
+      this.setState({...this.state, modify: true});
+      if (category === 0) update = StudentAPI.main.edit({language: form.language, level: form.level, target: form.target, content: form.content});
+      else if (category === 1) update = NativeAPI.main.edit({time: form.time, content: form.content});
+      else update = ForeignAPI.main.edit({content: form.content});
+      update.then(res => {
+        hide();
+        this.setState({...this.state, modify: false});
+        if (res.code === 0) {
+          message.success("个人信息修改成功！");
+        }
+        else {
+          message.error("个人信息修改失败！");
+        }
+      });
+    }
     return (
       <div key="0" className={styles.settings}>
+        <div className={styles.item}>
+          <p className={styles.title}>修改个人信息</p>
+          {category === 0 ? <>
+            <Select className={styles.input} defaultValue={this.state.information.language} placeholder="想学习的语种" onChange={handleSelect.bind(this, "language")}>
+              {CONST.language.map((v, i) => <Select.Option key={i} value={i}>{v}</Select.Option>)}
+            </Select>
+            <Select className={styles.input} defaultValue={this.state.information.level} placeholder="目前的水平" onChange={handleSelect.bind(this, "level")}>
+              {CONST.level.map((v, i) => <Select.Option key={i} value={i}>{v}</Select.Option>)}
+            </Select>
+            <Select className={styles.input} defaultValue={this.state.information.target} placeholder="最希望锻炼的能力" onChange={handleSelect.bind(this, "target")}>
+              {CONST.target.map((v, i) => <Select.Option key={i} value={i}>{v}</Select.Option>)}
+            </Select>
+          </> : null}
+          {category === 1 ? <>
+            <Input className={styles.input} name="time" prefix={<Icon type="clock-circle"/>} placeholder="可预约时间" onChange={handleChange} defaultValue={this.state.information.time} />
+          </> : null}
+          <Input className={styles.input} name="content" prefix={<Icon type="tags"/>} placeholder="简短介绍一下自己吧！" onChange={handleChange} defaultValue={this.state.information.content} />
+          <Button className={styles.input} type="primary" onClick={handleModify} loading={this.state.modify}>确定</Button>
+        </div>
         <div className={styles.item}>
           <p className={styles.title}>修改密码</p>
           <Input className={styles.input} type="password" name="oldpassword" prefix={<Icon type="lock"/>} placeholder="旧密码" onChange={handleChange} disabled={this.state.password} />
