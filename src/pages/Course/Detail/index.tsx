43a6796bd4bee4b140456206890ff25a0c41680a
chenghaopeng <i@chper.cn>
2020-03-06 20:07:24 +0800

    课程详细显示价格；完成购买

--- src/pages/Course/Detail/index.tsx
+++ src/pages/Course/Detail/index.tsx
@@ -1,10 +1,13 @@
 import React from "react";
 import styles from "./index.module.less";
 import GeneralAPI, { CourseDetail, Video, VideoTitle, ForeignBrief, ForeignDetail } from "../../../services/GeneralAPI";
-import { Spin, Empty, Tag, Collapse, Rate, Icon } from "antd";
+import { Spin, Empty, Tag, Collapse, Rate, Icon, Button, Modal, message, notification } from "antd";
 import UserDescriptions, { CONST } from "../../../components/UserDescriptions";
 import QueueAnim from "rc-queue-anim";
 import { RouteComponentProps, withRouter } from "react-router";
+import Price, { calcPrice } from "../../../components/Price";
+import store from "../../../store";
+import StudentAPI from "../../../services/StudentAPI";
 
 export interface DetailCourseConfig {
   cid: number,
@@ -34,6 +37,41 @@ class DetailCourse extends React.Component<{config: DetailCourseConfig} & RouteC
       else this.setState({status: -1});
     });
   }
+  componentDidMount() {
+    if (this.props.config.buying) {
+      const checkForBuy = setInterval(() => {
+        if (this.state.status !== 0) return;
+        this.buyCourse();
+        clearInterval(checkForBuy);
+      }, 500);
+    }
+  }
+  buyCourse = () => {
+    if (!store.getState().UserReducer.loged) { message.error("请先登录后购买！"); return; }
+    if (store.getState().UserReducer.session.category !== 0) { message.error("只有学生身份才能购买课程！"); return; }
+    const data = this.state.data as CourseDetail;
+    const onOk = () => {
+      StudentAPI.course.buy({cid: this.props.config.cid}).then(res => {
+        if (res.code === 0) notification.success({
+          message: "课程购买成功",
+          description: "你可以在“已购课程”中找到刚才购买的“" + data.name + "”，开始你的学习之旅吧！",
+          icon: <Icon type="smile" style={{ color: "green" }} />,
+        });
+        else message.error("购买失败，请检查你是否已购买过本课程，以及余额是否充足！");
+      });
+    }
+    Modal.confirm({
+      title: "即将购买",
+      content: <>
+        <div>课程名：{data.name}</div>
+        <div>价格：{calcPrice(data.cost, data.discount)}元</div>
+        <div>完成购买后你将可以学习本课程，是否确认购买？</div>
+      </>,
+      okText: "确认购买",
+      cancelText: "我再看看",
+      onOk,
+    });
+  }
   render() {
     let component: JSX.Element;
     if (this.state.status > 0) component = <Spin size="large" />;
@@ -51,6 +89,12 @@ class DetailCourse extends React.Component<{config: DetailCourseConfig} & RouteC
               <UserDescriptions className={styles.teacher} title="" information={this.state.foreign}></UserDescriptions>
             </Collapse.Panel>
           </Collapse>
+          <div key="pay" className={styles.pay}>
+            <div className={styles.buttons}>
+              {this.props.config.buy ? <Button className={styles.buy} type="primary" onClick={this.buyCourse}>购买</Button> : null}
+            </div>
+            <Price cost={data.cost} discount={data.discount} extra />
+          </div>
         </QueueAnim>
       );
     }
