a39f428395f8b5130996ce6b098596ea6e51cfa2
chenghaopeng <i@chper.cn>
2020-03-06 19:44:26 +0800

    抽离价格计算为函数

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -2,6 +2,10 @@ import React from "react";
 import styles from "./index.module.less";
 import { Icon } from "antd";
 
+export const calcPrice = (cost: number, discount: number) : number => {
+  return Math.floor(cost * (100 - discount) / 100);
+}
+
 class Price extends React.Component<{cost: number, discount: number, extra?: boolean}> {
   render() {
     const { cost, discount, extra } = this.props;
@@ -9,7 +13,7 @@ class Price extends React.Component<{cost: number, discount: number, extra?: boo
       <div className={styles.whole}>
         {extra ? <div className={styles.discount}><Icon type="arrow-down" />{discount}%</div> : null}
         {discount > 0 ? <div className={styles.former}>{cost}</div> : null}
-        <div className={styles.now}>{discount > 0 ? (cost * (100 - discount) / 100).toFixed(0) : cost}</div>
+        <div className={styles.now}>{calcPrice(cost, discount)}</div>
         {extra ? <div className={styles.yuan}>元</div> : null}
       </div>
     );
