de2f77bc7f42a7402cf74427757add0fe1e84d17
chenghaopeng <i@chper.cn>
2020-03-06 23:14:21 +0800

    首页轮播

--- src/pages/Main/index.tsx
+++ src/pages/Main/index.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import styles from './index.module.less';
 import Combiner from '../../components/Combiner';
+import "react-responsive-carousel/lib/styles/carousel.min.css";
+import { Carousel } from "react-responsive-carousel";
 
 class Main extends React.Component {
   render() {
     return Combiner(
       <div className={styles.container}>
-        Main
+        <div className={styles.whole}>
+          <Carousel className={styles.carousel} showThumbs={false} showStatus={false} autoPlay infiniteLoop emulateTouch interval={5000}>
+            <div><img src={require("../../themes/images/main/carousel/jianjie.png")} alt="" /></div>
+            <div><img src={require("../../themes/images/main/carousel/shizi.png")} alt="" /></div>
+            <div><img src={require("../../themes/images/main/carousel/kecheng.png")} alt="" /></div>
+          </Carousel>
+        </div>
       </div>
     );
   }
