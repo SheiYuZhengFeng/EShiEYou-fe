0562c53a412d1d3ba4104f9942f85b19d01bb3da
chenghaopeng <i@chper.cn>
2020-03-06 11:37:37 +0800

    所有课程页面添加跳转详细课程页面

--- src/pages/Course/All/index.tsx
+++ src/pages/Course/All/index.tsx
@@ -5,10 +5,12 @@ import { message, Skeleton, Tag, Button } from "antd";
 import { CONST } from "../../../components/UserDescriptions";
 import store from "../../../store";
 import { allCourseAction } from "../../../actions/CourseAction";
+import { RouteComponentProps, withRouter } from "react-router";
+import QueueAnim from "rc-queue-anim";
 
 const COLOR = ["orange", "green"];
 
-class AllCourse extends React.Component<{buy: boolean}, {courses: CourseBrief[]}> {
+class AllCourse extends React.Component<RouteComponentProps & {buy: boolean}, {courses: CourseBrief[]}> {
   constructor(props: any) {
     super(props);
     this.state = {courses: store.getState().CourseReducer.allcourse};
@@ -26,6 +28,13 @@ class AllCourse extends React.Component<{buy: boolean}, {courses: CourseBrief[]}
       }
     });
   }
+  toBuy = (id: number, e: React.MouseEvent<HTMLInputElement>) => {
+    e.stopPropagation(); //禁止冒泡
+    this.props.history.push("/course/" + id + "/buy");
+  }
+  toDetail = (id: number) => {
+    this.props.history.push("/course/" + id);
+  }
   render() {
     const { state } = this;
     if (Object.keys(state.courses).length === 0) {
@@ -33,16 +42,16 @@ class AllCourse extends React.Component<{buy: boolean}, {courses: CourseBrief[]}
       return <div className={styles.whole}><Skeleton className={styles.skeleton} active /></div>;
     }
     return (
-      <div className={styles.whole}>
-        <div className={styles.courses}>
+      <QueueAnim className={styles.whole} animConfig={[{opacity: [1, 0], translateY: [0, 10]}, {opacity: [1, 0], translateY: [0, -10]}]}>
+        <div key="courses" className={styles.courses}>
           {state.courses.map((v, i) => 
-            <div className={styles.course}>
+            <div key={i} className={styles.course} onClick={this.toDetail.bind(this, v.cid)}>
               <div className={styles.name + " " + styles.hidden}><Tag color={COLOR[v.category]}>{CONST.language[v.category]}</Tag>{v.name}</div>
               <div className={styles.time}>{new Date(v.starttime * 1000).toLocaleString()}</div>
               <div className={styles.content}>{v.content.length > 100 ? (v.content.substr(0, 100) + "...") : v.content}</div>
               <div className={styles.bottom}>
                 <div className={styles.buttons}>
-                  {this.props.buy ? <Button size={"small"} type="primary">购买</Button> : null}
+                  {this.props.buy ? <Button size={"small"} type="primary" onClick={this.toBuy.bind(this, v.cid)}>购买</Button> : null}
                   <Button size={"small"}>查看详细</Button>
                 </div>
                 <div className={styles.price}>
@@ -55,9 +64,9 @@ class AllCourse extends React.Component<{buy: boolean}, {courses: CourseBrief[]}
             </div>
           )}
         </div>
-      </div>
+      </QueueAnim>
     );
   }
 }
 
-export default AllCourse;
+export default withRouter(AllCourse);
