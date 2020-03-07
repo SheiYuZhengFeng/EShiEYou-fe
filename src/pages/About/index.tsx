7b70433700c4e102a438b6fbfefd51ea136067b9
chenghaopeng <i@chper.cn>
2020-03-07 13:19:57 +0800

    关于我们

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -2,11 +2,35 @@ import React from "react";
 import styles from "./index.module.less";
 import Combiner from "../../components/Combiner";
 
+const Values: {img: string, title: string}[] = [
+  {
+    img: require("../../themes/images/about/zhuanzhu.png"),
+    title: "专 注",
+  },
+  {
+    img: require("../../themes/images/about/dingzhi.png"),
+    title: "定 制",
+  },
+  {
+    img: require("../../themes/images/about/gongxiang.png"),
+    title: "共 享",
+  },
+];
+
 class About extends React.Component {
   render() {
     return Combiner(
       <div className={styles.container}>
-        关于我们
+        <div className={styles.whole}>
+          <div className={styles.values}>
+            {Values.map(v => (
+              <div className={styles.value}>
+                <img src={v.img} alt={v.title} />
+                <div className={styles.title}>{v.title}</div>
+              </div>
+            ))}
+          </div>
+        </div>
       </div>
     );
   }
