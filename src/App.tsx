fdf3e4e1c59adf5b5d109a9a8380e61a2ee37ed2
chenghaopeng <i@chper.cn>
2020-03-15 15:27:20 +0800

    移动最外层容器到 Combiner 组件

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,6 @@
 import React from 'react';
-import styles from './App.module.less';
 import Router from './router';
 
-const App = () => {
-  console.log(styles);
-  return (
-    <div className={styles.whole}>
-      <Router />
-    </div>
-  );
-}
+const App = () => <Router />;
 
 export default App;
--- src/components/Combiner/index.tsx
+++ src/components/Combiner/index.tsx
@@ -1,14 +1,15 @@
 import React from 'react';
+import styles from './index.module.less';
 import Navigator from '../Navigator';
 import Copyright from '../Copyright';
 
 const Combiner = (component: JSX.Element) => {
   return (
-    <>
+    <div className={styles.whole}>
       <Navigator />
       {component}
       <Copyright />
-    </>
+    </div>
   );
 }
 
