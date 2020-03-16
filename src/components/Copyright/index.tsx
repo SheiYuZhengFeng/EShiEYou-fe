891765f750f09c57a947e012c634233d4b4c8838
chenghaopeng <i@chper.cn>
2020-03-16 09:59:59 +0800

    版权信息修改

--- src/components/Copyright/index.tsx
+++ src/components/Copyright/index.tsx
@@ -5,7 +5,7 @@ class Copyright extends React.Component {
   render() {
     return (
       <div className={styles.container + ' ' + styles.whole}>
-        <p>E师亦友平台，是南京大学“谁语争锋”团队创建的专注职业小语种学习的在线学习平台。</p>
+        <p>E师亦友平台，是南京大学“谁语争锋”团队创建的专注小语种教育的在线学习平台。</p>
         <p>©2020-2020 谁语争锋团队 版权所有</p>
       </div>
     );
