afc8e224ca8ba552f57c4827fa3bfb77725745f1
chenghaopeng <i@chper.cn>
2020-03-19 15:32:43 +0800

    钱以分为单位

--- src/components/Price/index.tsx
+++ src/components/Price/index.tsx
@@ -3,7 +3,7 @@ import styles from "./index.module.less";
 import { Icon } from "antd";
 
 export const calcPrice = (cost: number, discount: number) : number => {
-  return Math.floor(cost * discount / 100);
+  return Math.floor(cost * discount / 100) / 100;
 }
 
 class Price extends React.Component<{cost: number, discount: number, extra?: boolean}> {
@@ -12,7 +12,7 @@ class Price extends React.Component<{cost: number, discount: number, extra?: boo
     return (
       <div className={styles.whole}>
         {extra ? <div className={styles.discount}><Icon type="arrow-down" />{(100 - discount)}%</div> : null}
-        {discount > 0 ? <div className={styles.former}>{cost}</div> : null}
+        {discount < 100 ? <div className={styles.former}>{calcPrice(cost, 100)}</div> : null}
         <div className={styles.now}>{calcPrice(cost, discount)}</div>
         {extra ? <div className={styles.yuan}>元</div> : null}
       </div>
