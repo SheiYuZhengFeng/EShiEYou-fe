e28257a6aeb0d9cb8fc0b4ac944e3b32fc14adf9
chenghaopeng <i@chper.cn>
2020-03-20 22:59:52 +0800

    添加 BillReducer 到根Reducer

--- src/reducers/index.tsx
+++ src/reducers/index.tsx
@@ -3,12 +3,14 @@ import UserReducer from "./UserReducer";
 import CourseReducer from "./CourseReducer";
 import OrderReducer from "./OrderReduer";
 import MailReducer from "./MailReducer";
+import BillReducer from "./BillReducer";
 
 const RootReducer = combineReducers({
   UserReducer: UserReducer,
   CourseReducer: CourseReducer,
   OrderReducer: OrderReducer,
   MailReducer: MailReducer,
+  BillReducer: BillReducer,
 });
 
 export default RootReducer;
