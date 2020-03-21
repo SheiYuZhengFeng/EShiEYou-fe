ae611c20a0e9eaa80d7b8bb1dbadeeb7151a41f5
chenghaopeng <i@chper.cn>
2020-03-21 21:26:02 +0800

    改持久化数据流的白名单为黑名单，设置仅私信数据不持久化

--- src/store.tsx
+++ src/store.tsx
@@ -7,7 +7,7 @@ import storage from "redux-persist/lib/storage";
 const persistConfig = {
   key: "root",
   storage,
-  whitelist: ["UserReducer"],
+  blacklist: ["MailReducer"],
 };
 
 const persistedReducer = persistReducer(persistConfig, RootReducer);
