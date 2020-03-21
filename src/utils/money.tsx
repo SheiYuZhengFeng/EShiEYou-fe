ee473bad0c6776ffefed1d8cfae8dbd4dc00e9e7
chenghaopeng <i@chper.cn>
2020-03-21 21:13:22 +0800

    钱工具类添加有符号钱格式化函数

--- src/utils/money.tsx
+++ src/utils/money.tsx
@@ -5,3 +5,7 @@ export const calcPrice = (cost: number, discount: number) : number => {
 export const convertMoney = (money: number) : string => {
   return (money / 100).toFixed(2);
 }
+
+export const signedMoney = (money: number) : string => {
+  return money === 0 ? "0.00" : (money > 0 ? "+" : "") + convertMoney(money);
+}
