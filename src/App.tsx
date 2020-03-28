8a17c7ab3bc98b45f9d42c6d78342f774385f96b
chenghaopeng <i@chper.cn>
2020-03-28 15:48:32 +0800

    添加国际化组件与配置

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,25 @@
 import React from 'react';
 import Router from './router';
+import intl from "react-intl-universal";
 
-const App = () => <Router />;
+const locales = {
+    "zh-CN": require("./locales/zh-CN.json"),
+};
+
+class App extends React.Component<any, {initDone: boolean}> {
+  constructor(props: any) {
+    super(props);
+    this.state = {initDone: false};
+  }
+  componentDidMount() {
+    intl.init({
+      currentLocale: "zh-CN",
+      locales,
+    }).then(() => this.setState({initDone: true}));
+  }
+  render() {
+    return this.state.initDone && <Router />;
+  }
+}
 
 export default App;
