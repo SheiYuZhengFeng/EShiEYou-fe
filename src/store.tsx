6509d1f2ebc55ffd7021e691bd07eea1e1bac7b1
chenghaopeng <i@chper.cn>
2020-03-30 22:39:32 +0800

    修改 redux-devtools 的引入方式

--- src/store.tsx
+++ src/store.tsx
@@ -1,8 +1,9 @@
-import { createStore, compose, applyMiddleware } from "redux";
+import { createStore, applyMiddleware } from "redux";
 import ReduxThunk from "redux-thunk";
 import RootReducer from "./reducers";
 import { persistStore, persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
+import { composeWithDevTools } from "redux-devtools-extension";
 
 const persistConfig = {
   key: "root",
@@ -12,15 +13,7 @@ const persistConfig = {
 
 const persistedReducer = persistReducer(persistConfig, RootReducer);
 
-const store = createStore(persistedReducer, 
-  (String(navigator.platform).indexOf("Linux") > -1) ? 
-  compose(
-    applyMiddleware(ReduxThunk),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__()
-  ) :
-  compose(
-    applyMiddleware(ReduxThunk),
-  ));
+const store = createStore(persistedReducer, composeWithDevTools(applyMiddleware(ReduxThunk)));
 
 export const persistor = persistStore(store);
 
